ee74cd42fb81277eace8c2f5c63fa03e596d8fb4
Hroki <hiroki_12.22_goal@ace.ocn.ne.jp>
2019-07-18 21:44:03 +0900

    deproy-chat-space

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+server '52.199.43.85', user: 'ec2-user', roles: %w{app db web}
+
