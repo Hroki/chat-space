84aeb846d6a9b1a65f9c8ceaf8c23081ee15a008
Hroki <hiroki_12.22_goal@ace.ocn.ne.jp>
2019-07-20 13:04:19 +0900

    日本時間の表示の設定を行いました

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,12 @@ module ChatSpace
       g.test_framework false
       config.i18n.default_locale = :ja
     end
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
